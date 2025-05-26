37f8c0254523a9665f1b151f12824c447abb275c
Glauber <glaubero.souza.dev@gmail.com>
2025-05-26 13:46:49 -0300

    Inicio do projeto

---
+++ src/main/java/com/glauber/diario_financeiro/DiarioFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.glauber.diario_financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioFinanceiroApplication.class, args);
+	}
+
+}
